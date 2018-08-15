1222b7820e98d0279856e268e0a617efafb7b5a9
thiagomotax <thiago_mota2017@hotmail.com>
2018-08-15 17:24:10 -0300

    Adicionar item adicionado

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDPBnDQauv4_tgf7pq_ZaTwZaA5dFDrx5c",
+    authDomain: "fb-crud-58aad.firebaseapp.com",
+    databaseURL: "https://fb-crud-58aad.firebaseio.com",
+    projectId: "fb-crud-58aad",
+    storageBucket: "fb-crud-58aad.appspot.com",
+    messagingSenderId: "950217559800"
+  }; 
